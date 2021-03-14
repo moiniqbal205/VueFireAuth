d2498df684ba0f5af15cc2bdc25c062fb4944a51
Moin Iqbal <moiniqbal205@gmail.com>
2021-03-14 11:21:14 +1100

    1st commit: Vue Firebase Authentication app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKo8gI671wYPzNMcQxqXLw-vm4xY8iVpo",
+    authDomain: "vue-firebase-auth-41a4a.firebaseapp.com",
+    projectId: "vue-firebase-auth-41a4a",
+    storageBucket: "vue-firebase-auth-41a4a.appspot.com",
+    messagingSenderId: "121100426928",
+    appId: "1:121100426928:web:866dcf810f986da526a575"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      <router-link to="/">Return to home</router-link>
+    </p>
   </div>
 </template>
